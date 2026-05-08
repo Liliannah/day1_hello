64ddba6885696ca351df0b2462364703df3ca285
Liliannah <chepoisho05@gmail.com>
2026-05-08 15:58:02 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,6 @@
+name =input  ( "what is your name? " )
+age =input  ( "how old are you? " )
+age = int ( age )
+birth_year =  2026 - age
+print  ("hello, ", name )
+print  ("you were born in,", birth_year)
