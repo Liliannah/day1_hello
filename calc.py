9824611605004d983c744dc9271896112b0e4820
Liliannah <chepoisho05@gmail.com>
2026-05-10 13:21:24 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,3 +4,12 @@ age = int ( age )
 birth_year =  2026 - age
 print  ("hello, ", name )
 print  ("you were born in,", birth_year)
+
+if age >=60:
+  print("you're senior Citizen! Respect.")
+elif age >=18:
+  print("you're an adult in Kenya.you can vote
+!")
+  else:
+  print("you're still a youth.Keep coding!") 
+  print("thanks for using calculator!")
